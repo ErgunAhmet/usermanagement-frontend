0b99256a55a9464b29b1d546e827ecc92c7e3820
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-09-05 21:22:59 +0200

    user service

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -1,1 +1,32 @@
-export class User{}
+export class User {
+  // @ts-ignore
+  public id: number;
+  // @ts-ignore
+  public userId: string;
+  public firstName: string;
+  public lastName: string;
+  public username: string;
+  public email: string;
+  // @ts-ignore
+  public logInDateDisplay: Date;
+  // @ts-ignore
+  public joinDate: Date;
+  // @ts-ignore
+  public profileImageUrl: string;
+  public active: boolean;
+  public notLocked: boolean;
+  public role: string;
+  public authorities: [];
+
+
+  constructor() {
+    this.firstName = '';
+    this.lastName = '';
+    this.username = '';
+    this.email = '';
+    this.active = false;
+    this.notLocked = false;
+    this.role = '';
+    this.authorities = [];
+  }
+}
